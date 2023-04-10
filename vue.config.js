df0e13f068b12e52e8dfd3e626a29391a1ac30fd
hibi01 <coder101011@outlook.com>
2023-04-10 10:20:46 +0800

    commit

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    disableHostCheck: true
+  },
   transpileDependencies: true,
   lintOnSave: false,
   configureWebpack: {
